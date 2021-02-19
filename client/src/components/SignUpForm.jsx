a8b0717c45c4c455217c342359ab673bb3ee51bc
chrisalopez1337 <chrisalopez@outlook.com>
2021-02-19 15:43:45 -0800

    Added all the fields for the sign up form and the event handlers

--- client/src/components/SignUpForm.jsx
+++ client/src/components/SignUpForm.jsx
@@ -18,6 +18,24 @@ export default function SignUpForm() {
             <form>
                 <label htmlFor="username">Enter a Username</label>
                 <input type="text" name="username" value={username} onChange={handleChange} />
+
+                <label htmlFor="email">Enter your Email</label>
+                <input type="email" name="email" value={email} onChange={handleChange} />
+
+                <label htmlFor="password">Enter your Password</label>
+                <input type="password" name="password" value={password} onChange={handleChange} />
+
+                <label htmlFor="verifyPassword">Confirm your password</label>
+                <input type="password" name="verifyPassword" value={verifyPassword} onChange={handleChange} />
+
+                <label htmlFor="summoner_name">Enter your summoner name</label>
+                <input type="text" name="summoner_name" value={summoner_name} onChange={handleChange} />
+                
+                <label htmlFor="region">Choose your region</label>
+                <select name="region" value={region} onChange={handleChange}>
+                    <option value="">Select an option</option>
+                    <option value="NA">NA</option>
+                </select>
             </form>
         </>
     );
