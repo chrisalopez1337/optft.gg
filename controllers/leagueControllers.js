6e8c4d6c51e6f6ca904233673d14d8ac85cc73a8
chrisalopez1337 <chrisalopez@outlook.com>
2021-02-20 18:59:15 -0800

    Added routes to query for basic summoner data by username, getSummonerBySummonerName

--- controllers/leagueControllers.js
+++ controllers/leagueControllers.js
@@ -1,7 +1,18 @@
 const TftQuery = require('tft-query');
+const apiKey = require('../config');
 
 module.exports = {
-    test: () => {
-        console.log('test');
+    getSummonerBySummonerName: async (req, res) => {
+        try {
+            const region = 'NA' // Default NA for now, but going to change to be dynamic later
+            const payload = { summonerName: req.params.summoner_name };
+            const config = { region, payload, apiKey, useRedis: false, redisOptions: false };
+            const tft = new TftQuery(config)
+            const data = await tft.getSummonerBySummonerName();
+            res.status(200).send(data);
+        } catch (err) {
+            console.log(err);
+            res.status(500).send(err);
+        }
     }
 }
--- server/leagueRoutes.js
+++ server/leagueRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const leagueRouter = express.Router();
+const leagueControllers = require('../controllers/leagueControllers.js');
 
-leagueRouter.get('/summoner/by-summoner-name/:summoner_name', (req, res) => { res.sendStatus(200)});
+leagueRouter.get('/summoner/by-summoner-name/:summoner_name', leagueControllers.getSummonerBySummonerName);
 
 module.exports = leagueRouter;
