e04b654c1452588531b2d1f476ff2aef2ed4ff7f
chrisalopez1337 <chrisalopez@outlook.com>
2021-02-21 20:51:15 -0800

    Fixed minor syntax error in database schema

--- database/index.js
+++ database/index.js
@@ -15,11 +15,11 @@ const userSchema = new mongoose.Schema({
     password: String,
     summoner_name: String,
     region: String,
-    lpRecord: { type: Object, default: { completeLpData: [] weeks: { dateHere: [] }}, // May want to consider splitting the arrays for this one 
+    lpRecord: { type: Object, default: { completeLpData: [], weeks: { dateHere: [] }}}, // May want to consider splitting the arrays for this one 
     matchHistory: { type: Object, default: { allMatches: { 1: [] }}},
     lastMatchIndex: { type: Number, default: 1 },
     lastGameId: { type: String, default: 'null' },
-})
+});
 
 const Users = mongoose.model('Users', userSchema);
 
