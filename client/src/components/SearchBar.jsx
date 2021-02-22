8d7a7dc0d82c087788d1a011ed19dffec17fe6b0
chrisalopez1337 <chrisalopez@outlook.com>
2021-02-22 12:42:24 -0800

    Added search bar and updates general info for a summoner, need to add messaging as well as storing the data upon a search.

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -23,31 +23,51 @@ const SearchItemWrapper = styled.div`
 `;
 
 
-export default function SearchBar() {
+export default function SearchBar({ setUserData, userData }) {
     // Field data
-    const [fields, setFields] = useState({ searchName: '', searchRegion: '' });
+    const [fields, setFields] = useState({ searchName: '', searchRegion: 'NA' });
     const { searchName, searchRegion } = fields;
     // Field handler
     function handleFields(e) {
         const { target } = e;
-        const { name, value } = e;
+        const { name, value } = target;
         setFields({...fields, [name]: value});
+    }
+    // User messaging
+    const [successMessage, setSuccessMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    // Handle search
+    function handleSearch() {
+        console.time('Search')
+        if (searchName === '' || searchRegion === '') {
+            setErrorMessage('Make sure you select a summoner name and region.');
+            return;
+        }
+
+        axios.get(`/api/leagues/allInfo/by-summmoner-name/${searchName}`)
+            .then(({ data }) => {
+                setUserData(data);
+            })
+            .catch(err => console.log(err));
     }
     return (
         <MainWrapper>
-        <SearchBarWrapper>
-            <SearchItemWrapper>
-                <label htmlFor="searchName">Summoner Name</label>
-                <input type="text" name="searchName" value={searchName} onChange={handleFields} />
-            </SearchItemWrapper>
+            <SearchBarWrapper>
             
-            <SearchItemWrapper>
-                <label htmlFor="searchRegion">Region</label>
-                <select value={searchRegion} onChange={handleFields}>
-                    <option value="NA">NA</option>
-                </select>
-            </SearchItemWrapper>
-        </SearchBarWrapper>
+                <SearchItemWrapper>
+                    <label htmlFor="searchName">Summoner Name</label>
+                    <input type="text" name="searchName" value={searchName} onChange={handleFields} />
+                </SearchItemWrapper>
+            
+                <SearchItemWrapper>
+                    <label htmlFor="searchRegion">Region</label>
+                    <select value={searchRegion} onChange={handleFields}>
+                        <option value="NA">NA</option>
+                    </select>
+                </SearchItemWrapper>
+                <button onClick={() => handleSearch()}>Search</button>
+            </SearchBarWrapper>
         </MainWrapper>
     );
 }
