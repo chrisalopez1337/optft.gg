27227ef9f42cb97e22b5ce8b3d422ba5b4325921
chrisalopez1337 <chrisalopez@outlook.com>
2021-02-22 13:04:34 -0800

    Added logic to conditionally render the new GeneralSummonerInfo component, however I do want to add some kind of user messaging to notify them the data is loading.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 // Components
 import Header from './Header.jsx';
 import SearchBar from './SearchBar.jsx';
+import GeneralSummonerInfo from './GeneralSummonerInfo.jsx';
 // Utils
 import { getItem } from '../utils/localStorage.js';
 
@@ -28,10 +29,23 @@ export default function App() {
             .catch(err => console.log(err));
     }, []);
 
+    // Conditional rendering for General summoner info, can def clean this up later.
+    let renderGeneralInfo;
+    if (userData) {
+        if (userData.name) {
+            renderGeneralInfo = <GeneralSummonerInfo userData={userData} />
+        } else {
+            renderGeneralInfo = <></>;
+        }
+    } else {
+        renderGeneralInfo = <></>;
+    }
+
     return (
         <AppWraper>
             <Header setUserData={setUserData} userData={userData} />
             <SearchBar setUserData={setUserData} userData={userData} />
+            {renderGeneralInfo}
         </AppWraper>
     );
 }
---
+++ client/src/components/GeneralSummonerInfo.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+const MainWrapper = styled.div`
+    display: flex;
+    flex-direction: column; 
+    padding: 25px;
+`;
+
+const InnerWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    border: 2px solid #e38f10;
+    border-radius: 7px;
+    padding: 10px;
+`;
+
+export default function GeneralSummonerInfo({ userData }) {
+    return (
+        <MainWrapper>
+            <InnerWrapper>
+                <h1>{userData.name}</h1>
+            </InnerWrapper>
+        </MainWrapper>
+    );
+}
+
+/* Notes
+ *
+ * Fields ______
+ * name
+ * tier
+ * division
+ * leaguePoints
+ * losses
+ * wins
+ * summonerLevel
+ * pofileIconId
+ * _____________
+ * These are currently the only fields we probably care about, may add more later, but for now just want to statically display this information.
+ */
