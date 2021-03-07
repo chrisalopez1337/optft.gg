d10fed1718aadb31c800ecf10f2681d6310c2cb7
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-06 17:05:36 -0800

    Added layout to display general rank and W/L ratio upon searching a summoner

--- client/src/components/GeneralSummonerInfo.jsx
+++ client/src/components/GeneralSummonerInfo.jsx
@@ -1,24 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-const MainWrapper = styled.div`
+const OuterWrapper = styled.div`
     display: flex;
-    flex-direction: column; 
-    padding: 25px;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 `;
 
 const InnerWrapper = styled.div`
     display: flex;
-    flex-direction: column;
+    align-items: left;
+    justify-content: left;
+    flex-direction: row;
     border: 2px solid #e38f10;
     border-radius: 7px;
     padding: 10px;
+    min-width: 300px;
 `;
 
-const PlayerTitleWrapper = styled.div`
+const ColumnHolder = styled.div`
     display: flex;
+    align-items: left;
+    justify-content: left;
+    flex-direction: column;
+    padding: 10px;
+`;
+
+const Section = styled.div`
+    font-size: 18px;
+    color: white;
+`;
+
+const Main = styled.div`
+    color: #e38f10;
+    font-size: 20px;
+    margin-left: 10px;
+`;
+
+const Row = styled.div`
+    display: flex;
+    margin: 10px;
     flex-direction: row;
-    justify-content: space-around;
 `;
 
 export default function GeneralSummonerInfo({ searchedUser, analyzedData }) {
@@ -27,13 +50,39 @@ export default function GeneralSummonerInfo({ searchedUser, analyzedData }) {
     const { averagedData, playerSearchedData } = analyzedData;
     console.log(analyzedData)
     return (
-        <MainWrapper>
+        <OuterWrapper>
+            <h1>{name}</h1>
             <InnerWrapper>
-                <PlayerTitleWrapper>
-                    <h1>{name}</h1>
-                </PlayerTitleWrapper>
+                <ColumnHolder>
+                    <Row>
+                        <Section>Tier:</Section>
+                        <Main>{tier}</Main>
+                    </Row>
+
+                    <Row>
+                        <Section>Rank:</Section>
+                        <Main>{rank}</Main>
+                    </Row>
+
+                    <Row>
+                        <Section>LP:</Section>
+                        <Main>{leaguePoints}</Main>
+                    </Row>
+                </ColumnHolder>
+
+                <ColumnHolder>
+                    <Row>
+                        <Section>Wins:</Section>
+                        <Main>{wins}</Main>
+                    </Row>
+
+                    <Row>
+                        <Section>Losses:</Section>
+                        <Main>{losses}</Main>
+                    </Row>
+                </ColumnHolder>
             </InnerWrapper>
-        </MainWrapper>
+        </OuterWrapper>
     );
 }
 
