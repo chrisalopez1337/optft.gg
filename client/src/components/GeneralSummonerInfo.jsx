7bf47de27a06c15c0c8b4a72bd048117d95b722c
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-06 17:49:50 -0800

    Added a players trait data and the peers average data

--- client/src/components/GeneralSummonerInfo.jsx
+++ client/src/components/GeneralSummonerInfo.jsx
@@ -48,6 +48,27 @@ export default function GeneralSummonerInfo({ searchedUser, analyzedData }) {
     console.log(searchedUser);
     const { name, profileIconId, rank, tier, leaguePoints, losses, wins, summonerLevel } = searchedUser;
     const { averagedData, playerSearchedData } = analyzedData;
+    const { traits } = playerSearchedData;
+    // Convert traits into an array for mapping
+    const playerTraits = [];
+    const formatName = (str) => {
+        if (str.indexOf('_') > -1) {
+            return str.split('_')[1];
+        } else {
+            return str;
+        }
+    };
+    for (const key in traits) {
+        const obj = 
+            {
+                name: formatName(key),
+                wins: traits[key].wins,
+                losses: traits[key].losses,
+            }
+        playerTraits.push(obj);
+    };
+    console.log(playerTraits);
+    const { averageDamageToPlayers, averageGoldLeft, averageLastRound, averageLevel, averagePlacement, averagePlayersEliminated, averageTimeEliminated, averageTraitData } = averagedData;
     console.log(analyzedData)
     return (
         <OuterWrapper>
@@ -114,7 +135,7 @@ export default function GeneralSummonerInfo({ searchedUser, analyzedData }) {
                 <ColumnHolder>
                     <Row>
                         <Section>Average Players Eliminated:</Section>
-                        <Main>{playerSearchedData.averagePlayersElminiated}</Main>
+                        <Main>{playerSearchedData.averagePlayersEliminated}</Main>
                     </Row>
 
                     <Row>
@@ -125,6 +146,60 @@ export default function GeneralSummonerInfo({ searchedUser, analyzedData }) {
                     <Row>
                         <Section>Average Damage To Players:</Section>
                         <Main>{playerSearchedData.averageDamageToPlayers}</Main>
+                    </Row>
+                </ColumnHolder>
+            </InnerWrapper>
+                <h1>Your Trait Data</h1>
+            <InnerWrapper>
+                <ColumnHolder>
+                {playerTraits.map(obj => {
+                    return (
+                        <Row>
+                            <Section>{obj.name}</Section>
+                            <Main> Wins: {obj.wins} Losses: {obj.losses} Ratio: {Math.floor(obj.wins / (obj.wins + obj.losses) * 100)}%</Main>
+                        </Row>
+                    );
+                })}
+                </ColumnHolder>
+            </InnerWrapper>
+                <h1>Peers Average Data</h1>
+            <InnerWrapper>
+                <ColumnHolder>
+                    <Row>
+                        <Section>Average Level:</Section>
+                        <Main>{Math.floor(averageLevel)}</Main>
+                    </Row>
+
+                    <Row>
+                        <Section>Average Gold Left:</Section>
+                        <Main>{Math.floor(averageGoldLeft)}</Main>
+                    </Row>
+
+                    <Row>
+                        <Section>Average Placement:</Section>
+                        <Main>{Math.floor(averagePlacement)}</Main>
+                    </Row>
+
+                    <Row>
+                        <Section>Average Last Round:</Section>
+                        <Main>{Math.floor(averageLastRound)}</Main>
+                    </Row>
+                </ColumnHolder>
+
+                <ColumnHolder>
+                    <Row>
+                        <Section>Average Players Eliminated:</Section>
+                        <Main>{Math.floor(averagePlayersEliminated)}</Main>
+                    </Row>
+
+                    <Row>
+                        <Section>Average Time Eliminated:</Section>
+                        <Main>{Math.floor(averageTimeEliminated / 60)}mins</Main>
+                    </Row>
+
+                    <Row>
+                        <Section>Average Damage To Players:</Section>
+                        <Main>{Math.floor(averageDamageToPlayers)}</Main>
                     </Row>
                 </ColumnHolder>
             </InnerWrapper>
